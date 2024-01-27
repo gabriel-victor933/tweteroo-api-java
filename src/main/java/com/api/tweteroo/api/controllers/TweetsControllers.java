884bdf296fda99b6b2f85cd5aa80960d5ed41b28
gabriel-victor933 <gabrielvictor933@usp.br>
2024-01-27 10:55:42 -0300

    feat: adding rest endpoints

--- src/main/java/com/api/tweteroo/api/controllers/TweetsControllers.java
+++ src/main/java/com/api/tweteroo/api/controllers/TweetsControllers.java
@@ -1,5 +1,33 @@
 package com.api.tweteroo.api.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController("tweets")
 public class TweetsControllers {
+    
+    @GetMapping
+    public SomeData getTweets() {
+        return new SomeData();
+    }
+
+    @PostMapping
+    public SomeEnityData postTweet(@RequestBody SomeEnityData entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
+    
+    @GetMapping("/user/{id}")
+    public SomeData getMethodName(@PathVariable Long id) {
+        return new SomeData();
+    }
+    
     
 }
--- src/main/java/com/api/tweteroo/api/controllers/UsersControllers.java
+++ src/main/java/com/api/tweteroo/api/controllers/UsersControllers.java
@@ -1,5 +1,17 @@
 package com.api.tweteroo.api.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController("users")
 public class UsersControllers {
+    
+    @PostMapping
+    public SomeEnityData postUser(@RequestBody SomeEnityData entity) {
+        
+        return "entity";
+    }
     
 }
