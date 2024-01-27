e5cc3d92e14ee1999afcc164d80b4eb9414ab3b8
gabriel-victor933 <gabrielvictor933@usp.br>
2024-01-27 14:44:27 -0300

    feat: adding route get tweet by userId

--- src/main/java/com/api/tweteroo/api/controllers/TweetsControllers.java
+++ src/main/java/com/api/tweteroo/api/controllers/TweetsControllers.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,25 +46,20 @@ public class TweetsControllers {
         if(!userOptinal.isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
         }
-
-        try {
             TweetModel createdTweet = new TweetModel(tweet.getTweet(),userOptinal.get());
             tweetsRepository.save(createdTweet);
 
             return ResponseEntity.status(HttpStatus.CREATED).body(createdTweet);
 
-        } catch(Exception e){ 
-            System.out.println(e.getClass());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("error");
-        }
 
 
     }
     
-/*     @GetMapping("/user/{id}")
+    @GetMapping("/user/{id}")
     public ResponseEntity<Object> getTweetsByUserId(@PathVariable Long id) {
-        List<TweetModel> tweets = tweetsRepository.findAll();
+        List<TweetModel> tweets = tweetsRepository.findTweetByUserId(id);
+
         return ResponseEntity.status(HttpStatus.OK).body(tweets);
-    } */
+    }
     
 }
--- src/main/java/com/api/tweteroo/api/repositories/TweetsRepository.java
+++ src/main/java/com/api/tweteroo/api/repositories/TweetsRepository.java
@@ -1,11 +1,18 @@
 package com.api.tweteroo.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.api.tweteroo.api.models.TweetModel;
 
 @Repository
 public interface TweetsRepository extends JpaRepository<TweetModel, Long> {
+
+    @Query(value = "SELECT * FROM tweets WHERE user_id = :userId",nativeQuery = true)
+    List<TweetModel> findTweetByUserId(@Param("userId") Long userId);
     
 }
